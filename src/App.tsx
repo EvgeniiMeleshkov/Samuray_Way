ae53561e5b52e6b435c12750c7deae2349de49ff
Evgenii <emeleshkov@mail.ru>
2022-05-20 18:42:17 +0300

    README.md changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className={'header'}>
-                <img className={'img'}
-                     src={'https://images.fineartamerica.com/images/artworkimages/medium/3/programmer-coding-samurai-japan-debugging-funny-lisa-stronzi-transparent.png'}/>
-            </header>
-            <nav className={'nav'}>
-                <div>Profile</div>
-                <div>Messages</div>
-                <div>Users</div>
-                <div>Music</div>
-                <div>Settings</div>
-            </nav>
-            <div className={'content'}>
-                <div>
-                    <img className={'content-img'}
-                         src={'https://www.textillia.com/sites/default/files/styles/large/public/img/2022/01/14/1Samurai%20LogoV1pattern.jpg?itok=I2y422PV'}/>
-                </div>
-                <div>ava + description</div>
-                <div>
-                    My Posts
-                    <div>
-                        New Post
-                    </div>
-                    <div>
-                        Post 1: Hey, Samurai!
-                    </div>
-                    <div>
-                        Post 2: It-incubator
-                    </div>
-
-                </div>
-            </div>
-
+           <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
