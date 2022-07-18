f73df9b1bab54ccd7575f1d55ede99b4e1647b48
Evgenii <emeleshkov@mail.ru>
2022-07-18 21:26:46 +0300

    47 done

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
-import {store} from './redux/redux_store'
+import {store} from './redux/redux_store';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
 import React from 'react';
-import './index.css';
-import {rerenderEntireTree} from './render';
+import {Provider} from 'react-redux';
 
-
-
-store.subscribe(rerenderEntireTree)
-rerenderEntireTree()
+    ReactDOM.render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
