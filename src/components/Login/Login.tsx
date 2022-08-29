4485c55b2eea17163377cbcabb03d8cbc91be6af
Evgenii <emeleshkov@mail.ru>
2022-08-30 02:38:55 +0300

    75 in progress

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,9 +3,29 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            ...Login...
+            <h1>Login</h1>
+            <LoginForm/>
         </div>
     );
 };
 
 export default Login;
+
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'email'}/>
+            </div>
+            <div>
+                <input placeholder={'password'}/>
+            </div>
+            <div>
+                <input type={'checkbox'}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
